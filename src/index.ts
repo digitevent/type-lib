3a6dfea3ce734b6d91c82dcdd89c55146f016759
Thomas Moreira <moreirathomas97@gmail.com>
2022-12-02 09:32:06 +0100

    feat(poc): Nominal type (#1)
    * ci: fix package-lock name [skip ci]
    
    * feat: Nominal  type and constructor
    
    * test: runtime tests
    
    * chore: expose api
    
    * test: add type testing
    
    * ci: fix steps order
    
    * fix: exclude test-d.ts files from compilation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { Err, isErr, isOk, Ok, ResultType } from './result'
 export type { Result } from './result'
+export { nominal } from './nominal'
+export type { Nominal } from './nominal'
---
+++ src/nominal.ts
@@ -0,0 +1,51 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
+declare const __brand: unique symbol
+// eslint-disable-next-line @typescript-eslint/naming-convention
+declare const __type: unique symbol
+
+interface Brand<T, B extends string> {
+  [__brand]: B
+  [__type]: T
+}
+
+/**
+ * `Nominal<T, B>` transforms `T` into a nominal type of `T`.
+ *
+ * See this official gist for more explanation on nominal typing:
+ * [permalink](https://github.com/microsoft/TypeScript-Website/blob/da25328eb3ec6e1b2a0ff67b4f9595295f96a38c/packages/playground-examples/copy/en/TypeScript/Language%20Extensions/Nominal%20Typing.ts).
+ * @example
+ * ```ts
+ * import { Nominal, nominal } from '@digitevent-org/type-lib'
+ *
+ * type MyString = Nominal<string, 'MyString'>
+ * const newMyString = (v: string) => nominal<MyString>(v)
+ * ```
+ */
+export type Nominal<T, B extends string> = T & Brand<T, B>
+
+interface Flavoring<T, B> {
+  [__brand]?: B
+  // Remember the information about the internal type so it
+  // can be inferred from the nominal type through Unbrand.
+  [__type]?: T
+}
+
+// A Flavor type can be implicitly converted to its base type.
+// See https://spin.atomicobject.com/2018/01/15/typescript-flexible-nominal-typing/
+// for inspiration.
+type Flavor<T, B> = T & Flavoring<T, B>
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Unbrand<T> = T extends Flavor<infer X, any> ? X : T
+
+/**
+ * Create a nominal type from a base type.
+ * @example
+ * ```ts
+ * import { Nominal, nominal } from '@digitevent-org/type-lib'
+ *
+ * type MyString = Nominal<string, 'MyString'>
+ * const newMyString = (v: string) => nominal<MyString>(v)
+ * ```
+ */
+export const nominal = <T>(value: Unbrand<T>): T => value as T
